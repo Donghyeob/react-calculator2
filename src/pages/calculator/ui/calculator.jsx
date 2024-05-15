095ac3c006da90f75f21a929bc5b311b23e55536
Donghyeob <donghyeob.k@gmail.com>
2024-05-16 01:17:20 +0900

    button directory rename

--- src/pages/calculator/ui/calculator.jsx
+++ src/pages/calculator/ui/calculator.jsx
@@ -1,6 +1,6 @@
 import { ScanInput } from '../../../features/scan';
 import { PrintDisplay } from '../../../features/print';
-import { Buttons } from '../../../features/button';
+import { Buttons } from '../../../features/buttons/index.jsx';
 import { calculator } from './styled.css';
 
 export const Calculator = () => {
